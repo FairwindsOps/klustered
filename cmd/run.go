b7e43471102b76c0216a9b978c50b559ff4b75ae
Andrew Suderman <andrew@sudermanjr.com>
2021-08-04 11:16:11 -0600

    License and such

--- cmd/run.go
+++ cmd/run.go
@@ -1,3 +1,16 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package cmd
 
 import (
@@ -33,7 +46,7 @@ var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "Runs the klustered program for breaking the cluster.",
 	Long: `Run sets up an http server on 8443 that does a few things:
-	
+
 - Responds to a mutating admission webhook on /mutate
 - Responds to a validating admissino webhook on /admission
 - Responds to health checks on /
