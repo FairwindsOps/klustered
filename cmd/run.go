9352aad0e7ab9b3e7fe0452ec9f31a98d5e912b4
Andrew Suderman <andrew@sudermanjr.com>
2021-08-03 18:29:50 -0600

    Create mutating webhook configuration automagically (#3)
    * auto-register webhook
    
    * remove mutating config
    
    * call the watcher
    
    * manifests and fix ticker
    
    * more manifests

--- cmd/run.go
+++ cmd/run.go
@@ -26,6 +26,8 @@ import (
 
 	a "github.com/fairwindsops/klustered/pkg/admission"
 	m "github.com/fairwindsops/klustered/pkg/mutate"
+	r "github.com/fairwindsops/klustered/pkg/register"
+
 	"github.com/spf13/cobra"
 	"k8s.io/klog/v2"
 )
@@ -47,6 +49,12 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		watcher, err := r.NewWatcher(crt)
+		if err != nil {
+			klog.Fatal(err)
+		}
+		watcher.Run()
+
 		mux := http.NewServeMux()
 
 		mux.HandleFunc("/", handleRoot)
